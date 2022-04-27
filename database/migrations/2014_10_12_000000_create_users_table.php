8dd4e9ac2accb8b090085fb60f5351d7c2bac117
= <gael.stervinou@finelia.fr>
2022-04-27 15:34:26 +0200

    Init du projet

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('role_id')->default(0);
+            $table->integer('role_id')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
